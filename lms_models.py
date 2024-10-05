65659c7a3424bfacb6596f8e6ab18a2b5a6848b0
Sos Sokleng <33617233+SosSokleng99@users.noreply.github.com>
2024-10-05 17:15:35 +0700

    Update lms_models.py Starting Model

--- lms_models.py
+++ lms_models.py
@@ -20,7 +20,7 @@ class Genre(models.Model):
 
     def get_absolute_url(self):
         """Returns the url to access a particular genre instance."""
-        return reverse('genre-detail', args=[str(self.id)])
+        return reverse('genre_detail', args=[str(self.id)])
 
     class Meta:
         constraints = [
@@ -36,9 +36,6 @@ class Genre(models.Model):
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books.
-    # Author as a string rather than object because it hasn't been declared yet in file.
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13,
@@ -47,23 +44,13 @@ class Book(models.Model):
                                       '">ISBN number</a>')
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
-    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    language = models.ForeignKey(
-        'Language', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         ordering = ['title', 'author']
 
-    def display_genre(self):
-        """Creates a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join([genre.name for genre in self.genre.all()[:3]])
-
-    display_genre.short_description = 'Genre'
-
     def get_absolute_url(self):
         """Returns the url to access a particular book record."""
-        return reverse('book-detail', args=[str(self.id)])
+        return reverse('book_detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
@@ -89,7 +76,7 @@ class BookInstance(models.Model):
     @property
     def is_overdue(self):
         """Determines if the book is overdue based on due date and current date."""
-        return bool(self.due_back and date.today() > self.due_back)
+        return bool(date.today() > self.due_back)
 
     LOAN_STATUS = (
         ('d', 'Maintenance'),
@@ -107,12 +94,11 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
-        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def get_absolute_url(self):
         """Returns the url to access a particular book instance."""
-        return reverse('bookinstance-detail', args=[str(self.id)])
+        return reverse('bookinstance_detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        return self.book.title
